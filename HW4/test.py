917baeb61f6f0d0b12e252629fcf1161f4ed9eef
Shakib7 <sarwar@purdue.edu>
2017-09-21 14:09:28 -0400

    Create test.py

---
+++ HW4/test.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from neural_network import NeuralNetwork
+from nn_img2num import NnImg2Num
+from my_img2num import MyImg2Num
+import numpy
+import random
+import torch
+import torchvision
+from torch.autograd import Variable
+
+
+def test():
+	train_loader = torchvision.datasets.MNIST('../data_MNIST', train=True, download=True, transform=True,target_transform=True)
+	epoch = 10;    learning_rate = 0.5;    batch_size = 100;    numCls = 10;	iteration = len(train_loader) / batch_size;
+	input = (torch.squeeze(train_loader.train_data.view(iteration, batch_size, 1, 784), 2)).type(torch.FloatTensor)
+
+	#net = NnImg2Num()
+	#net.train()
+	#print('forward output', net.forward(input[0]))
+	#print('call output', net(input[0]))
+
+
+	netMy = MyImg2Num();
+	netMy.train()
+	print('forward output', netMy.forward((input[0])))
+	print('call output', netMy((input[0])))
+
+test()
